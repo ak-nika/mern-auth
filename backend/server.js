de9349a2346a9d6d5ca8b1d9a3f2be71c968a73d
ak-nika <akingbayiojo@gmail.com>
2025-05-15 14:19:26 +0100

    Connection to database sucessful

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const app = require("./app");
 const dotenv = require("dotenv");
+const connectDB = require("./config/mongodb");
 
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 
+connectDB();
 app.listen(port, () => {
   console.log(`Server running on port ${port}...`);
 });
