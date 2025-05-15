4ce433de26659d6986f596be86f487cb04105e42
ak-nika <akingbayiojo@gmail.com>
2025-05-15 14:46:08 +0100

    Register function complete

---
+++ backend/controllers/authController.js
@@ -0,0 +1,38 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
+
+exports.register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.status(400).json({
+      status: "Failed",
+      message: "Please fill in all fields",
+    });
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+    res.status(201).json({ status: "Success", data: user });
+  } catch (error) {
+    res.status(500).json({ status: "Failed", message: error.message });
+  }
+};
---
+++ backend/models/userModel.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please add a name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please add an email"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please add a password"],
+  },
+  verifyOtp: {
+    type: String,
+    default: "",
+    select: false,
+  },
+  verifyOtpExpireAt: {
+    type: Number,
+    default: 0,
+    select: false,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  resetOtp: {
+    type: String,
+    default: "",
+    select: false,
+  },
+  resetOtpExpireAt: {
+    type: Number,
+    default: 0,
+    select: false,
+  },
+});
+
+const userModel = mongoose.model("User", userSchema);
+module.exports = userModel;
